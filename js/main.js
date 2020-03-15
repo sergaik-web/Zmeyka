a4935ff204b9df05fb29ca4eb578ad357ea424f5
sergaik-web <60810704+sergaik-web@users.noreply.github.com>
2020-03-15 15:08:34 +0300

    Минимум правок

--- js/main.js
+++ js/main.js
@@ -10,41 +10,37 @@ document.addEventListener("keydown", function (e) {
        console.log(key);
 });
 
+let getCord = () => {
+        let cordV = Math.round(Math.random()*100/5);
+        let cordG = Math.round(Math.random()*100/5);
+        return [cordV, cordG];
+};
+
 //создаём еду для змеи
 let createEat = () => {
-    let cordV = Math.round(Math.random()*100/5);
-    let cordG = Math.round(Math.random()*100/5);
-    console.log(cordV,cordG);
+    let coord = getCord();
+    console.log(coord);
     let crElem = document.createElement('div');
-    crElem.style.gridColumn = cordG;
-    crElem.style.gridRow = cordV;
+    crElem.style.gridColumn = coord[1];
+    crElem.style.gridRow = coord[0];
     crElem.style.backgroundColor = 'red';
     document.getElementById('game-display').append(crElem);
 };
 createEat();
 
-let addOrgan = () => {
-    let cordV = vertical-1;
-    let cordG = gorizont-1;
-    let crElem = document.createElement('div');
-    crElem.style.gridColumn = cordG;
-    crElem.style.gridRow = cordV;
-};
-
-
 //Нажатие на кнопку
 butStart.addEventListener("click", function () {
     function moveZmey() {
-        if (key == 'ArrowRight'){
+        if (key === 'ArrowRight'){
             (gorizont >= 20) ? gorizont = 1 : gorizont++;
             zmey.style.gridColumn = gorizont;
-        } else if (key == 'ArrowLeft'){
+        } else if (key === 'ArrowLeft'){
             (gorizont <= 1) ? gorizont = 20 : gorizont--;
             zmey.style.gridColumn = gorizont;
-        } else if (key == 'ArrowUp'){
+        } else if (key === 'ArrowUp'){
             (vertical <= 1) ? vertical = 20 : vertical--;
             zmey.style.gridRow = vertical;
-        } else if (key == 'ArrowDown'){
+        } else if (key === 'ArrowDown'){
             (vertical >= 20) ? vertical = 1 : vertical++;
             zmey.style.gridRow = vertical;
         }
